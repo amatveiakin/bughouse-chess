b9ea36604862723e1e9b2b275b44267f41f77cf8
Andrei Matveiakin <a.matveiakin@gmail.com>
2022-04-25 17:25:03 +0300

    Clear input and error on state transition; make client unit-testable

--- src/client.rs
+++ src/client.rs
@@ -26,7 +26,13 @@ enum IncomingEvent {
     Tick,
 }
 
-enum ClientState {
+enum EventReaction {
+    Continue,
+    ExitOk,
+    ExitWithError(String),
+}
+
+enum ContestState {
     Uninitialized,
     Lobby { players: Vec<Player> },
     Game {
@@ -34,28 +40,197 @@ enum ClientState {
         game_confirmed: Option<BughouseGame>,  // state from the server, if different from `game`
         game_start: Option<Instant>,
     },
+    // TODO: Separate state for `GameOver`.
 }
 
-fn make_join_event(player_name: &str, team_name: &str) -> BughouseClientEvent {
-    BughouseClientEvent::Join {
-        player_name: player_name.to_owned(),
-        team: match team_name {
-            "red" => Team::Red,
-            "blue" => Team::Blue,
-            _ => panic!("Unexpected team: {}", team_name),
+// TODO: Factor out and unit-test.
+struct ClientState<'a, OutStream: io::Write> {
+    my_name: &'a str,
+    my_team: Team,
+    out_stream: &'a mut OutStream,
+    contest_state: ContestState,
+    command_error: Option<String>,
+    keyboard_input: String,
+}
+
+impl<'a, OutStream: io::Write> ClientState<'a, OutStream> {
+    pub fn new(my_name: &'a str, my_team: Team, out_stream: &'a mut OutStream) -> Self {
+        ClientState {
+            my_name,
+            my_team,
+            out_stream,
+            contest_state: ContestState::Uninitialized,
+            command_error: None,
+            keyboard_input: String::new(),
         }
+    }
+
+    pub fn contest_state(&self) -> &ContestState { &self.contest_state }
+    pub fn command_error(&self) -> &Option<String> { &self.command_error }
+    pub fn keyboard_input(&self) -> &String { &self.keyboard_input }
+
+    // Must be called exactly once before calling `apply_event`.
+    pub fn join(&mut self) -> io::Result<()> {
+        network::write_obj(self.out_stream, &BughouseClientEvent::Join {
+            player_name: self.my_name.to_owned(),
+            team: self.my_team,
+        })
+    }
+
+    pub fn apply_event(&mut self, event: IncomingEvent) -> io::Result<EventReaction> {
+        let mut command_to_execute = None;
+        match event {
+            IncomingEvent::Terminal(term_event) => {
+                if let term_event::Event::Key(key_event) = term_event {
+                    match key_event.code {
+                        term_event::KeyCode::Char(ch) => {
+                            self.keyboard_input.push(ch);
+                        },
+                        term_event::KeyCode::Backspace => {
+                            self.keyboard_input.pop();
+                        },
+                        term_event::KeyCode::Enter => {
+                            command_to_execute = Some(self.keyboard_input.trim().to_owned());
+                            self.keyboard_input.clear();
+                        },
+                        _ => {},
+                    }
+                }
+            },
+            IncomingEvent::Network(net_event) => {
+                use BughouseServerEvent::*;
+                match net_event {
+                    Error{ message } => {
+                        return Ok(EventReaction::ExitWithError(message));
+                    },
+                    LobbyUpdated{ players } => {
+                        let new_players = players;
+                        match self.contest_state {
+                            ContestState::Lobby{ ref mut players } => {
+                                *players = new_players;
+                            },
+                            _ => {
+                                self.new_contest_state(ContestState::Lobby {
+                                    players: new_players
+                                });
+                            },
+                        }
+                    },
+                    GameStarted{ chess_rules, bughouse_rules, starting_grid, players } => {
+                        let player_map = BughouseGame::make_player_map(
+                            players.iter().map(|(p, board_idx)| (Rc::new(p.clone()), *board_idx))
+                        );
+                        self.new_contest_state(ContestState::Game {
+                            game: BughouseGame::new_with_grid(
+                                chess_rules, bughouse_rules, starting_grid, player_map
+                            ),
+                            game_confirmed: None,
+                            game_start: None,
+                        });
+                    },
+                    TurnMade{ player_name, turn_algebraic, time, game_status } => {
+                        if let ContestState::Game{
+                            ref mut game, ref mut game_confirmed, ref mut game_start
+                        } = self.contest_state {
+                            assert!(game.status() == BughouseGameStatus::Active, "Cannot make turn: game over");
+                            if game_start.is_none() {
+                                // TODO: Sync client/server times better; consider NTP
+                                *game_start = Some(Instant::now());
+                            }
+                            if player_name == self.my_name {
+                                *game = game_confirmed.take().unwrap();
+                            }
+                            let turn_result = game.try_turn_by_player_from_algebraic(
+                                &player_name, &turn_algebraic, time
+                            );
+                            turn_result.unwrap_or_else(|err| {
+                                panic!("Impossible turn: {}, error: {:?}", turn_algebraic, err)
+                            });
+                            assert_eq!(game_status, game.status());
+                        } else {
+                            panic!("Cannot make turn: no game in progress")
+                        }
+                    },
+                    GameOver{ time, game_status } => {
+                        if let ContestState::Game{ ref mut game, ref mut game_confirmed, .. } = self.contest_state {
+                            if let Some(game_confirmed) = game_confirmed.take() {
+                                *game = game_confirmed;
+                            }
+                            assert!(game.status() == BughouseGameStatus::Active);
+                            assert!(game_status != BughouseGameStatus::Active);
+                            // TODO: Make sure this is synced with flag.
+                            game.set_status(game_status, time);
+                        } else {
+                            panic!("Cannot record game result: no game in progress")
+                        }
+                    },
+                }
+            },
+            IncomingEvent::Tick => {
+                // Any event triggers repaint, so no additional action is required.
+            },
+        }
+
+        if let Some(cmd) = command_to_execute {
+            self.command_error = None;
+            if cmd == "quit" {
+                network::write_obj(self.out_stream, &BughouseClientEvent::Leave)?;
+                return Ok(EventReaction::ExitOk);
+            }
+            if let ContestState::Game{ ref mut game, ref mut game_confirmed, .. } = self.contest_state {
+                if game.player_is_active(&self.my_name).unwrap() {
+                    assert!(game_confirmed.is_none());
+                    *game_confirmed = Some(game.clone());
+                    // Don't try to advance the clock: server is the source of truth for flag defeat.
+                    // TODO: Fix time recorded in order to show accurate local time before the server confirmed the move.
+                    //   Problem: need to avoid recording flag defeat prematurely.
+                    let clock = game.player_board(&self.my_name).unwrap().clock();
+                    let turn_start = clock.turn_start().unwrap_or(GameInstant::game_start());
+                    let turn_result = game.try_turn_by_player_from_algebraic(
+                        &self.my_name, &cmd, turn_start
+                    );
+                    match turn_result {
+                        Ok(_) => {
+                            network::write_obj(self.out_stream, &BughouseClientEvent::MakeTurn {
+                                turn_algebraic: cmd
+                            })?;
+                        },
+                        Err(err) => {
+                            *game_confirmed = None;
+                            // TODO: FIX: Screen is not updated while an error is shown.
+                            self.command_error = Some(format!("Illegal turn '{}': {:?}", cmd, err));
+                        },
+                    }
+                } else {
+                    self.keyboard_input = cmd;
+                }
+            } else {
+                self.command_error = Some(format!("unknown command: '{}'", cmd));
+            }
+        }
+        Ok(EventReaction::Continue)
+    }
+
+    fn new_contest_state(&mut self, contest_state: ContestState) {
+        self.contest_state = contest_state;
+        self.command_error = None;
+        self.keyboard_input.clear();
     }
 }
 
 pub fn client_main(config: ClientConfig) -> io::Result<()> {
     let my_name = config.player_name.trim();
+    let my_team = match config.team.as_str() {
+        "red" => Team::Red,
+        "blue" => Team::Blue,
+        _ => panic!("Unexpected team: {}", config.team),
+    };
     let server_addr = (config.server_address.as_str(), network::PORT).to_socket_addrs().unwrap().collect_vec();
     println!("Connecting to {:?}...", server_addr);
     let mut net_stream = TcpStream::connect(&server_addr[..])?;
     // TODO: Test if nodelay helps.  Should it be set on both sides or just one?
     //   net_stream.set_nodelay(true)?;
     let mut net_read_stream = net_stream.try_clone()?;
-    network::write_obj(&mut net_stream, &make_join_event(my_name, &config.team))?;
     #[allow(unused_variables)] let config = ();  // shouldn't be used anymore;  TODO: how to do this properly?
 
     let mut stdout = io::stdout();
@@ -88,129 +263,18 @@ pub fn client_main(config: ClientConfig) -> io::Result<()> {
         }
     });
 
-    let mut client_state = ClientState::Uninitialized;
-    let mut error_message: Option<String> = None;
-    let mut keyboard_input = String::new();
+    let mut client_state = ClientState::new(my_name, my_team, &mut net_stream);
+    client_state.join()?;
     for event in rx {
-        // TODO: What should happen to error_message after state transition?
-        let mut command_to_execute = None;
-        match event {
-            IncomingEvent::Terminal(term_event) => {
-                if let term_event::Event::Key(key_event) = term_event {
-                    match key_event.code {
-                        term_event::KeyCode::Char(ch) => {
-                            keyboard_input.push(ch);
-                        },
-                        term_event::KeyCode::Backspace => {
-                            keyboard_input.pop();
-                        },
-                        term_event::KeyCode::Enter => {
-                            command_to_execute = Some(keyboard_input.trim().to_owned());
-                            keyboard_input.clear();
-                        },
-                        _ => {},
-                    }
-                }
-            },
-            IncomingEvent::Network(net_event) => {
-                use BughouseServerEvent::*;
-                match net_event {
-                    Error{ message } => {
-                        execute!(io::stdout(), terminal::LeaveAlternateScreen)?;
-                        writeln!(stdout, "Fatal error: {}", message)?;
-                        std::process::exit(1);
-                    },
-                    LobbyUpdated{ players } => {
-                        client_state = ClientState::Lobby{ players };
-                    },
-                    GameStarted{ chess_rules, bughouse_rules, starting_grid, players } => {
-                        let player_map = BughouseGame::make_player_map(
-                            players.iter().map(|(p, board_idx)| (Rc::new(p.clone()), *board_idx))
-                        );
-                        client_state = ClientState::Game {
-                            game: BughouseGame::new_with_grid(
-                                chess_rules, bughouse_rules, starting_grid, player_map
-                            ),
-                            game_confirmed: None,
-                            game_start: None,
-                        };
-                    },
-                    TurnMade{ player_name, turn_algebraic, time, game_status } => {
-                        if let ClientState::Game{
-                            ref mut game, ref mut game_confirmed, ref mut game_start
-                        } = client_state {
-                            assert!(game.status() == BughouseGameStatus::Active, "Cannot make turn: game over");
-                            if game_start.is_none() {
-                                // TODO: Sync client/server times better; consider NTP
-                                *game_start = Some(Instant::now());
-                            }
-                            if player_name == my_name {
-                                *game = game_confirmed.take().unwrap();
-                            }
-                            let turn_result = game.try_turn_by_player_from_algebraic(
-                                &player_name, &turn_algebraic, time
-                            );
-                            turn_result.unwrap_or_else(|err| {
-                                panic!("Impossible turn: {}, error: {:?}", turn_algebraic, err)
-                            });
-                            assert_eq!(game_status, game.status());
-                        } else {
-                            panic!("Cannot make turn: no game in progress")
-                        }
-                    },
-                    GameOver{ time, game_status } => {
-                        if let ClientState::Game{ ref mut game, ref mut game_confirmed, .. } = client_state {
-                            if let Some(game_confirmed) = game_confirmed.take() {
-                                *game = game_confirmed;
-                            }
-                            assert!(game.status() == BughouseGameStatus::Active);
-                            assert!(game_status != BughouseGameStatus::Active);
-                            // TODO: Make sure this is synced with flag.
-                            game.set_status(game_status, time);
-                        } else {
-                            panic!("Cannot record game result: no game in progress")
-                        }
-                    },
-                }
-            },
-            IncomingEvent::Tick => {
-                // Any event triggers repaint, so no additional action is required.
-            },
-        }
-
-        if let Some(cmd) = command_to_execute {
-            if cmd == "quit" {
-                network::write_obj(&mut net_stream, &BughouseClientEvent::Leave)?;
+        match client_state.apply_event(event)? {
+            EventReaction::Continue => {},
+            EventReaction::ExitOk => {
                 return Ok(());
-            }
-            if let ClientState::Game{ ref mut game, ref mut game_confirmed, .. } = client_state {
-                if game.player_is_active(my_name).unwrap() {
-                    assert!(game_confirmed.is_none());
-                    *game_confirmed = Some(game.clone());
-                    // Don't try to advance the clock: server is the source of truth for flag defeat.
-                    // TODO: Fix time recorded in order to show accurate local time before the server confirmed the move.
-                    //   Problem: need to avoid recording flag defeat prematurely.
-                    let clock = game.player_board(my_name).unwrap().clock();
-                    let turn_start = clock.turn_start().unwrap_or(GameInstant::game_start());
-                    let turn_result = game.try_turn_by_player_from_algebraic(
-                        my_name, &cmd, turn_start
-                    );
-                    match turn_result {
-                        Ok(_) => {
-                            error_message = None;
-                            network::write_obj(&mut net_stream, &BughouseClientEvent::MakeTurn {
-                                turn_algebraic: cmd
-                            })?;
-                        },
-                        Err(err) => {
-                            *game_confirmed = None;
-                            // TODO: FIX: Screen is not updated while an error is shown.
-                            error_message = Some(format!("Illegal turn: {:?}", err));
-                        },
-                    }
-                } else {
-                    keyboard_input = cmd;
-                }
+            },
+            EventReaction::ExitWithError(message) => {
+                execute!(io::stdout(), terminal::LeaveAlternateScreen)?;
+                writeln!(stdout, "Fatal error: {}", message)?;
+                std::process::exit(1);
             }
         }
 
@@ -219,11 +283,11 @@ pub fn client_main(config: ClientConfig) -> io::Result<()> {
         // TODO: Don't clear the board to avoid blinking.
         execute!(stdout, terminal::Clear(terminal::ClearType::FromCursorDown))?;
         let mut highlight_input = false;
-        match client_state {
-            ClientState::Uninitialized => {
+        match client_state.contest_state() {
+            ContestState::Uninitialized => {
                 writeln!(stdout, "Loading...")?;
             },
-            ClientState::Lobby{ ref players } => {
+            ContestState::Lobby{ ref players } => {
                 let mut teams: EnumMap<Team, Vec<String>> = enum_map!{ _ => vec![] };
                 for p in players {
                     teams[p.team].push(p.name.clone());
@@ -240,7 +304,7 @@ pub fn client_main(config: ClientConfig) -> io::Result<()> {
                     writeln!(stdout, "")?;
                 }
             },
-            ClientState::Game{ ref game, ref game_start, .. } => {
+            ContestState::Game{ ref game, ref game_start, .. } => {
                 let game_now = match game_start {
                     Some(t) => GameInstant::new(*t, now),
                     None => GameInstant::game_start(),
@@ -258,12 +322,13 @@ pub fn client_main(config: ClientConfig) -> io::Result<()> {
         // Simulate cursor: real cursor blinking is broken with Show/Hide.
         let show_cursor = now.duration_since(app_start_time).as_millis() % 1000 >= 500;
         let cursor = if show_cursor { '▂' } else { ' ' };
+        let input_with_cursor = format!("{}{}", client_state.keyboard_input(), cursor);
         let input_style = if highlight_input { style::Color::White } else { style::Color::DarkGrey };
         // TODO: Show input on a fixed line regardless of client_status.
-        write!(stdout, "{}", format!("{}{}", keyboard_input, cursor).with(input_style))?;
+        write!(stdout, "{}", input_with_cursor.with(input_style))?;
 
         writeln!(stdout, "\n")?;
-        if let Some(ref err) = error_message {
+        if let Some(ref err) = client_state.command_error() {
             writeln!(stdout, "{}", err.clone().with(style::Color::Red))?;
         }
     }
