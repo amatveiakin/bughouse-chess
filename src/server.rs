297a53f041e6aa7f00ceb8af34e0d2a200d4f017
Andrei Matveiakin <a.matveiakin@gmail.com>
2022-04-25 17:57:09 +0300

    Make server unit-testable

--- src/server.rs
+++ src/server.rs
@@ -20,7 +20,7 @@ enum IncomingEvent {
     Tick,
 }
 
-enum ServerState {
+enum ContestState {
     Lobby,
     Game {
         game: BughouseGame,
@@ -29,6 +29,163 @@ enum ServerState {
 }
 
 type Players = Vec<(Option<Player>, TcpStream)>;
+
+struct ServerState {
+    players: Players,
+    contest_state: ContestState,
+    new_client_tx: mpsc::Sender<IncomingEvent>,  // should redirect to `apply_event` input
+    chess_rules: ChessRules,
+    bughouse_rules: BughouseRules,
+}
+
+impl ServerState {
+    pub fn new(new_client_tx: mpsc::Sender<IncomingEvent>) -> Self {
+        ServerState {
+            players: vec![],
+            contest_state: ContestState::Lobby,
+            new_client_tx,
+            chess_rules: ChessRules {
+                starting_position: StartingPosition::FischerRandom,
+                time_control: TimeControl{ starting_time: Duration::from_secs(300) },
+            },
+            bughouse_rules: BughouseRules {
+                min_pawn_drop_row: SubjectiveRow::from_one_based(2),
+                max_pawn_drop_row: SubjectiveRow::from_one_based(6),
+                drop_aggression: DropAggression::NoChessMate,
+            },
+        }
+    }
+
+    // TODO: Better error handling
+    pub fn apply_event(&mut self, event: IncomingEvent) {
+        let now = Instant::now();
+        if let ContestState::Game{ ref mut game, game_start } = self.contest_state {
+            if let Some(game_start) = game_start {
+                let game_now = GameInstant::new(game_start, now);
+                game.test_flag(game_now);
+                if game.status() != BughouseGameStatus::Active {
+                    broadcast_event(&mut self.players, &BughouseServerEvent::GameOver {
+                        time: game_now,
+                        game_status: game.status(),
+                    }).unwrap();
+                    return;
+                }
+            }
+        }
+
+        match event {
+            IncomingEvent::ClientConnected(mut stream) => {
+                match self.contest_state {
+                    ContestState::Lobby => {
+                        assert!(self.players.len() < TOTAL_PLAYERS);
+                        let player_id = self.players.len();
+                        self.players.push((None, stream.try_clone().unwrap()));
+                        let tx_new = self.new_client_tx.clone();
+                        thread::spawn(move || {
+                            loop {
+                                let ev = network::parse_obj::<BughouseClientEvent>(
+                                    &network::read_str(&mut stream).unwrap()).unwrap();
+                                tx_new.send(IncomingEvent::ClientEvent(player_id, ev)).unwrap();
+                            }
+                        });
+                    },
+                    ContestState::Game{ .. } => {
+                        // TODO: Allow to reconnect
+                        network::write_obj(&mut stream, &BughouseServerEvent::Error {
+                            message: "Cannot connect: game has already started".to_owned(),
+                        }).unwrap();
+                    },
+                }
+            },
+            IncomingEvent::ClientEvent(player_id, event) => {
+                match event {
+                    BughouseClientEvent::Join{ player_name, team } => {
+                        if let ContestState::Lobby = self.contest_state {
+                            if self.players[player_id].0.is_some() {
+                                send_error(&mut self.players, player_id, "Cannot join: already joined".to_owned()).unwrap();
+                            } else {
+                                // TODO: Check name uniqueness
+                                if get_team_players(&self.players, team).count() >= TOTAL_PLAYERS_PER_TEAM {
+                                    send_error(&mut self.players, player_id, format!("Cannot join: team {:?} is full", team)).unwrap();
+                                } else {
+                                    println!("Player {} joined team {:?}", player_name, team);
+                                    self.players[player_id].0 = Some(Player {
+                                        name: player_name,
+                                        team,
+                                    });
+                                    let player_to_send = self.players.iter().filter_map(|(p, _)| p.clone()).collect_vec();
+                                    broadcast_event(&mut self.players, &BughouseServerEvent::LobbyUpdated {
+                                        players: player_to_send,
+                                    }).unwrap();
+                                }
+                            }
+                        } else {
+                            send_error(&mut self.players, player_id, "Cannot join: game has already started".to_owned()).unwrap();
+                        }
+                    },
+                    BughouseClientEvent::MakeTurn{ turn_algebraic } => {
+                        if let ContestState::Game{ ref mut game, ref mut game_start } = self.contest_state {
+                            if game_start.is_none() {
+                                *game_start = Some(now);
+                            }
+                            let game_now = GameInstant::new(game_start.unwrap(), now);
+                            let player_name = self.players[player_id].0.as_ref().unwrap().name.clone();
+                            let turn_result = game.try_turn_by_player_from_algebraic(
+                                &player_name, &turn_algebraic, game_now
+                            );
+                            if let Err(error) = turn_result {
+                                send_error(&mut self.players, player_id, format!("Impossible turn: {:?}", error)).unwrap();
+                            }
+                            broadcast_event(&mut self.players, &BughouseServerEvent::TurnMade {
+                                player_name: player_name.to_owned(),
+                                turn_algebraic,  // TODO: Rewrite turn to a standard form
+                                time: game_now,
+                                game_status: game.status(),
+                            }).unwrap();
+                            if game.status() != BughouseGameStatus::Active {
+                                return;
+                            }
+                        } else {
+                            send_error(&mut self.players, player_id, "Cannot make turn: no game in progress".to_owned()).unwrap();
+                        }
+                    },
+                    BughouseClientEvent::Leave => {
+                        broadcast_event(&mut self.players, &BughouseServerEvent::Error {
+                            message: "Oh no! Somebody left the party".to_owned(),
+                        }).unwrap();
+                    },
+                }
+            },
+            IncomingEvent::Tick => {
+                // Any event triggers state update, so no additional action is required.
+            },
+        }
+
+        if let ContestState::Lobby = self.contest_state {
+            assert!(self.players.len() <= TOTAL_PLAYERS);
+            if self.players.len() == TOTAL_PLAYERS && self.players.iter().all(|(p, _)| p.is_some()) {
+                let players_with_boards = assign_boards(&self.players);
+                let player_map = BughouseGame::make_player_map(
+                    players_with_boards.iter().map(|(p, board_idx)| (Rc::new(p.clone()), *board_idx))
+                );
+                let game = BughouseGame::new(
+                    self.chess_rules.clone(), self.bughouse_rules.clone(), player_map
+                );
+                let starting_grid = game.board(BughouseBoard::A).grid().clone();
+                self.contest_state = ContestState::Game {
+                    game,
+                    game_start: None,
+                };
+                broadcast_event(&mut self.players, &BughouseServerEvent::GameStarted {
+                    chess_rules: self.chess_rules.clone(),
+                    bughouse_rules: self.bughouse_rules.clone(),
+                    starting_grid,
+                    players: players_with_boards,
+                }).unwrap();
+            }
+        };
+    }
+}
 
 fn get_team_players(players: &Players, team: Team) -> impl Iterator<Item = &Player> {
     players.iter().filter_map(move |(player_or, _)| {
@@ -70,151 +227,6 @@ fn assign_boards(players: &Players) -> Vec<(Player, BughouseBoard)> {
     [make_team(Team::Red), make_team(Team::Blue)].concat()
 }
 
-// TODO: Better error handling
-fn game_main(tx: mpsc::Sender<IncomingEvent>, rx: mpsc::Receiver<IncomingEvent>) {
-    let chess_rules: ChessRules = ChessRules {
-        starting_position: StartingPosition::FischerRandom,
-        time_control: TimeControl{ starting_time: Duration::from_secs(300) },
-    };
-    let bughouse_rules: BughouseRules = BughouseRules {
-        min_pawn_drop_row: SubjectiveRow::from_one_based(2),
-        max_pawn_drop_row: SubjectiveRow::from_one_based(6),
-        drop_aggression: DropAggression::NoChessMate,
-    };
-
-    let mut players: Players = Vec::new();
-    let mut server_state = ServerState::Lobby;
-    for event in rx {
-        let now = Instant::now();
-        if let ServerState::Game{ ref mut game, game_start } = server_state {
-            if let Some(game_start) = game_start {
-                let game_now = GameInstant::new(game_start, now);
-                game.test_flag(game_now);
-                if game.status() != BughouseGameStatus::Active {
-                    broadcast_event(&mut players, &BughouseServerEvent::GameOver {
-                        time: game_now,
-                        game_status: game.status(),
-                    }).unwrap();
-                    return;
-                }
-            }
-        }
-
-        match event {
-            IncomingEvent::ClientConnected(mut stream) => {
-                match server_state {
-                    ServerState::Lobby => {
-                        assert!(players.len() < TOTAL_PLAYERS);
-                        let player_id = players.len();
-                        players.push((None, stream.try_clone().unwrap()));
-                        let tx_new = tx.clone();
-                        thread::spawn(move || {
-                            loop {
-                                let ev = network::parse_obj::<BughouseClientEvent>(
-                                    &network::read_str(&mut stream).unwrap()).unwrap();
-                                tx_new.send(IncomingEvent::ClientEvent(player_id, ev)).unwrap();
-                            }
-                        });
-                    },
-                    ServerState::Game{ .. } => {
-                        // TODO: Allow to reconnect
-                        network::write_obj(&mut stream, &BughouseServerEvent::Error {
-                            message: "Cannot connect: game has already started".to_owned(),
-                        }).unwrap();
-                    },
-                }
-            },
-            IncomingEvent::ClientEvent(player_id, event) => {
-                match event {
-                    BughouseClientEvent::Join{ player_name, team } => {
-                        if let ServerState::Lobby = server_state {
-                            if players[player_id].0.is_some() {
-                                send_error(&mut players, player_id, "Cannot join: already joined".to_owned()).unwrap();
-                            } else {
-                                // TODO: Check name uniqueness
-                                if get_team_players(&players, team).count() >= TOTAL_PLAYERS_PER_TEAM {
-                                    send_error(&mut players, player_id, format!("Cannot join: team {:?} is full", team)).unwrap();
-                                } else {
-                                    println!("Player {} joined team {:?}", player_name, team);
-                                    players[player_id].0 = Some(Player {
-                                        name: player_name,
-                                        team,
-                                    });
-                                    let player_to_send = players.iter().filter_map(|(p, _)| p.clone()).collect_vec();
-                                    broadcast_event(&mut players, &BughouseServerEvent::LobbyUpdated {
-                                        players: player_to_send,
-                                    }).unwrap();
-                                }
-                            }
-                        } else {
-                            send_error(&mut players, player_id, "Cannot join: game has already started".to_owned()).unwrap();
-                        }
-                    },
-                    BughouseClientEvent::MakeTurn{ turn_algebraic } => {
-                        if let ServerState::Game{ ref mut game, ref mut game_start } = server_state {
-                            if game_start.is_none() {
-                                *game_start = Some(now);
-                            }
-                            let game_now = GameInstant::new(game_start.unwrap(), now);
-                            let player_name = players[player_id].0.as_ref().unwrap().name.clone();
-                            let turn_result = game.try_turn_by_player_from_algebraic(
-                                &player_name, &turn_algebraic, game_now
-                            );
-                            if let Err(error) = turn_result {
-                                send_error(&mut players, player_id, format!("Impossible turn: {:?}", error)).unwrap();
-                            }
-                            broadcast_event(&mut players, &BughouseServerEvent::TurnMade {
-                                player_name: player_name.to_owned(),
-                                turn_algebraic,  // TODO: Rewrite turn to a standard form
-                                time: game_now,
-                                game_status: game.status(),
-                            }).unwrap();
-                            if game.status() != BughouseGameStatus::Active {
-                                return;
-                            }
-                        } else {
-                            send_error(&mut players, player_id, "Cannot make turn: no game in progress".to_owned()).unwrap();
-                        }
-                    },
-                    BughouseClientEvent::Leave => {
-                        broadcast_event(&mut players, &BughouseServerEvent::Error {
-                            message: "Oh no! Somebody left the party".to_owned(),
-                        }).unwrap();
-                    },
-                }
-            },
-            IncomingEvent::Tick => {
-                // Any event triggers state update, so no additional action is required.
-            },
-        }
-
-        if let ServerState::Lobby = server_state {
-            assert!(players.len() <= TOTAL_PLAYERS);
-            if players.len() == TOTAL_PLAYERS && players.iter().all(|(p, _)| p.is_some()) {
-                let players_with_boards = assign_boards(&players);
-                let player_map = BughouseGame::make_player_map(
-                    players_with_boards.iter().map(|(p, board_idx)| (Rc::new(p.clone()), *board_idx))
-                );
-                let game = BughouseGame::new(
-                    chess_rules.clone(), bughouse_rules.clone(), player_map
-                );
-                let starting_grid = game.board(BughouseBoard::A).grid().clone();
-                server_state = ServerState::Game {
-                    game,
-                    game_start: None,
-                };
-                broadcast_event(&mut players, &BughouseServerEvent::GameStarted {
-                    chess_rules: chess_rules.clone(),
-                    bughouse_rules: bughouse_rules.clone(),
-                    starting_grid,
-                    players: players_with_boards,
-                }).unwrap();
-            }
-        };
-    }
-    panic!("Unexpected end of events stream");
-}
-
 pub fn server_main() {
     let (tx, rx) = mpsc::channel();
     let tx_client_connected = tx.clone();
@@ -226,7 +238,11 @@ pub fn server_main() {
         }
     });
     thread::spawn(move || {
-        game_main(tx, rx);
+        let mut server_state = ServerState::new(tx);
+        for event in rx {
+            server_state.apply_event(event);
+        }
+        panic!("Unexpected end of events stream");
     });
 
     let listener = TcpListener::bind(("0.0.0.0", network::PORT)).unwrap();
