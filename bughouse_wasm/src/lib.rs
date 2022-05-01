cade1df29372355bef593f3e369a1513b9009b7d
Andrei Matveiakin <a.matveiakin@gmail.com>
2022-05-01 13:15:08 +0400

    Show player names, reserves and clocks in web client

--- bughouse_wasm/src/lib.rs
+++ bughouse_wasm/src/lib.rs
@@ -1,6 +1,10 @@
+// TODO: Shrink WASM file size.
+
 extern crate enum_map;
 extern crate instant;
+extern crate itertools;
 extern crate serde_json;
+extern crate strum;
 extern crate wasm_bindgen;
 
 extern crate bughouse_chess;
@@ -9,6 +13,8 @@ use std::sync::mpsc;
 
 use enum_map::{EnumMap, enum_map};
 use instant::Instant;
+use itertools::Itertools;
+use strum::{EnumIter, IntoEnumIterator};
 use wasm_bindgen::prelude::*;
 
 use bughouse_chess::*;
@@ -31,7 +37,6 @@ pub fn set_panic_hook() {
 pub struct WebClient {
     state: ClientState,
     server_rx: mpsc::Receiver<BughouseClientEvent>,
-    primary_board: Option<BughouseBoard>,
 }
 
 #[wasm_bindgen]
@@ -46,7 +51,6 @@ impl WebClient {
         WebClient {
             state: ClientState::new(my_name.to_owned(), my_team, server_tx),
             server_rx,
-            primary_board: None,
         }
     }
 
@@ -68,12 +72,12 @@ impl WebClient {
     pub fn process_server_event(&mut self, event: &str) {
         let game_was_active = matches!(self.state.contest_state(), ContestState::Game{ .. });
         self.state.process_server_event(serde_json::from_str(event).unwrap()).unwrap();
-        if !game_was_active {
-            if let ContestState::Game{ ref game_confirmed, .. } = self.state.contest_state() {
+        if let ContestState::Game{ ref game_confirmed, .. } = self.state.contest_state() {
+            if !game_was_active {
                 let my_name = self.state.my_name();
-                let (board_idx, force) = game_confirmed.find_player(my_name).unwrap();
-                render_game_area(force == Force::Black);
-                self.primary_board = Some(board_idx);
+                let (_, force) = game_confirmed.find_player(my_name).unwrap();
+                render_grids(force == Force::Black);
+                render_players();
             }
         }
     }
@@ -107,28 +111,57 @@ impl WebClient {
             ContestState::Game{ ref game_confirmed, ref local_turn, game_start } => {
                 let my_name = self.state.my_name();
                 let now = Instant::now();
-                let _game_now = GameInstant::from_maybe_active_game(*game_start, now).approximate();
+                let game_now = GameInstant::from_maybe_active_game(*game_start, now).approximate();
                 let game = game_local(my_name, game_confirmed, local_turn);
-                // TODO: Show clock and player names; show reserve
+                let (my_board_idx, my_force) = game.find_player(my_name).unwrap();
                 for (board_idx, board) in game.boards() {
-                    let is_primary = board_idx == self.primary_board.unwrap();
+                    let is_primary = board_idx == my_board_idx;
                     let web_board_idx = if is_primary { WebBoard::Primary } else { WebBoard::Secondary };
                     let grid = board.grid();
                     for coord in Coord::all() {
                         let sq = document.get_element_by_id(&square_id(web_board_idx, coord)).unwrap();
-                        sq.set_text_content(Some(&String::from(to_unicode_char(&grid[coord]))));
+                        let piece_str = match grid[coord] {
+                            Some(piece) => String::from(to_unicode_char(piece.kind, piece.force)),
+                            None => String::new(),
+                        };
+                        sq.set_text_content(Some(&piece_str));
+                    }
+                    for player_idx in WebPlayer::iter() {
+                        use WebPlayer::*;
+                        use WebBoard::*;
+                        let force = match (player_idx, web_board_idx) {
+                            (Bottom, Primary) | (Top, Secondary) => my_force,
+                            (Top, Primary) | (Bottom, Secondary) => my_force.opponent(),
+                        };
+                        let id_suffix = format!("{}-{}", board_id(web_board_idx), player_id(player_idx));
+                        let name_node = document.get_element_by_id(&format!("player-name-{}", id_suffix)).unwrap();
+                        name_node.set_text_content(Some(&board.player(force).name));
+                        let reserve_node = document.get_element_by_id(&format!("reserve-{}", id_suffix)).unwrap();
+                        reserve_node.set_text_content(Some(&reserve_string(board.reserve(force), force)));
+                        let clock_node = document.get_element_by_id(&format!("clock-{}", id_suffix)).unwrap();
+                        update_clock(board.clock(), force, game_now, &clock_node).unwrap();
                     }
                 }
-                info_string.set_text_content(None);
+                if game_confirmed.status() != BughouseGameStatus::Active {
+                    info_string.set_text_content(Some(&format!("Game over: {:?}", game_confirmed.status())));
+                } else {
+                    info_string.set_text_content(None);
+                }
             },
         }
     }
 }
 
-#[derive(Clone, Copy, PartialEq, Eq, Debug)]
+#[derive(Clone, Copy, PartialEq, Eq, Debug, EnumIter)]
 enum WebBoard {
     Primary,
     Secondary,
+}
+
+#[derive(Clone, Copy, PartialEq, Eq, Debug, EnumIter)]
+enum WebPlayer {
+    Top,
+    Bottom,
 }
 
 #[derive(Debug)]
@@ -151,20 +184,91 @@ fn remove_all_children(node: &web_sys::Node) -> Result<(), JsValue> {
 
 #[wasm_bindgen]
 pub fn init_page() {
-    render_game_area(false);
+    render_grids(false);
 }
 
-fn render_game_area(flip_forces: bool) {
-    let document = web_document();
-    let board_primary = document.get_element_by_id("board-primary").unwrap();
-    let board_secondary = document.get_element_by_id("board-secondary").unwrap();
-    render_grid(&board_primary, WebBoard::Primary, flip_forces).unwrap();
-    render_grid(&board_secondary, WebBoard::Secondary, flip_forces).unwrap();
+fn reserve_string(reserve: &Reserve, force: Force) -> String {
+    let mut stacks = Vec::new();
+    for (piece_kind, &amount) in reserve.iter() {
+        if amount > 0 {
+            stacks.push(String::from(to_unicode_char(piece_kind, force)).repeat(amount.into()));
+        }
+    }
+    stacks.iter().join(" ")
 }
 
-fn render_grid(node: &web_sys::Node, board_idx: WebBoard, flip_forces: bool) -> Result<(), JsValue> {
+fn div_ceil(a: u128, b: u128) -> u128 { (a + b - 1) / b }
+
+// TODO: Dedup against console client
+fn update_clock(clock: &Clock, force: Force, now: GameInstant, clock_node: &web_sys::Element) -> Result<(), JsValue> {
+    let is_active = clock.active_force() == Some(force);
+    let millis = clock.time_left(force, now).as_millis();
+    let sec = millis / 1000;
+    let separator = |s| if !is_active || millis % 1000 >= 500 { s } else { " " };
+    let clock_str = if sec >= 20 {
+        format!("{:02}{}{:02}", sec / 60, separator(":"), sec % 60)
+    } else {
+        format!("{:02}{}{}", sec, separator("."), div_ceil(millis, 100) % 10)
+    };
+    clock_node.set_text_content(Some(&clock_str));
+    if is_active {
+        clock_node.set_attribute("class", "clock clock-active")?;
+    } else if millis == 0 {
+        // Note. This will not apply to an active player, which is by design.
+        // When the game is over, all clocks stop, so no player is active.
+        // An active player can have zero time only in an online game client.
+        // In this case we shouldn't paint the clock red (which means defeat)
+        // before the server confirmed game result, because the game may have
+        // ended earlier on the other board.
+        clock_node.set_attribute("class", "clock clock-flag")?;
+    } else {
+        clock_node.set_attribute("class", "clock")?;
+    }
+    Ok(())
+}
+
+fn render_players() {
+    for board_idx in WebBoard::iter() {
+        for player_idx in WebPlayer::iter() {
+            render_player(board_idx, player_idx).unwrap();
+        }
+    }
+}
+
+fn render_player(board_idx: WebBoard, player_idx: WebPlayer) -> Result<(), JsValue> {
     let document = web_document();
-    remove_all_children(node)?;
+    let id_suffix = format!("{}-{}", board_id(board_idx), player_id(player_idx));
+    let node_id = format!("player-{}", id_suffix);
+    let node = document.get_element_by_id(&node_id).unwrap();
+    remove_all_children(&node)?;
+
+    let table = document.create_element("table")?;
+    table.set_attribute("class", "player-view")?;
+    let tr = document.create_element("tr")?;
+    for obj in ["player-name", "reserve", "clock"] {
+        let td = document.create_element("td")?;
+        td.set_attribute("id", &format!("{}-{}", obj, id_suffix))?;
+        td.set_attribute("class", obj)?;
+        tr.append_child(&td)?;
+    }
+    table.append_child(&tr)?;
+    node.append_child(&table)?;
+
+    Ok(())
+}
+
+fn render_grids(flip_forces: bool) {
+    for board_idx in WebBoard::iter() {
+        render_grid(board_idx, flip_forces).unwrap();
+    }
+}
+
+fn render_grid(board_idx: WebBoard, flip_forces: bool) -> Result<(), JsValue> {
+    let document = web_document();
+    let node_id = format!("board-{}", board_id(board_idx));
+    let node = document.get_element_by_id(&node_id).unwrap();
+    remove_all_children(&node)?;
+
     let table = document.create_element("table")?;
     for line in make_grid(board_idx, flip_forces) {
         let tr = document.create_element("tr")?;
@@ -221,12 +325,22 @@ fn make_grid(board_idx: WebBoard, mut flip_forces: bool) -> Vec<Vec<Square>> {
     }).collect()
 }
 
-fn square_id(board_idx: WebBoard, coord: Coord) -> String {
-    let prefix = match board_idx {
+fn board_id(idx: WebBoard) -> String {
+    match idx {
         WebBoard::Primary => "primary",
         WebBoard::Secondary => "secondary",
-    };
-    format!("{}-{}", prefix, coord.to_algebraic())
+    }.to_owned()
+}
+
+fn player_id(idx: WebPlayer) -> String {
+    match idx {
+        WebPlayer::Top => "top",
+        WebPlayer::Bottom => "bottom",
+    }.to_owned()
+}
+
+fn square_id(board_idx: WebBoard, coord: Coord) -> String {
+    format!("{}-{}", board_id(board_idx), coord.to_algebraic())
 }
 
 fn square_color_class(row: Row, col: Col) -> String {
@@ -238,37 +352,28 @@ fn square_color_class(row: Row, col: Col) -> String {
 }
 
 fn inner_square_size_class(board_idx: WebBoard) -> String {
-    match board_idx {
-        WebBoard::Primary => "sq-in-primary".to_owned(),
-        WebBoard::Secondary => "sq-in-secondary".to_owned(),
-    }
+    format!("sq-in-{}", board_id(board_idx))
 }
 
 fn outer_square_size_class(board_idx: WebBoard) -> String {
-    match board_idx {
-        WebBoard::Primary => "sq-out-primary".to_owned(),
-        WebBoard::Secondary => "sq-out-secondary".to_owned(),
-    }
+    format!("sq-out-{}", board_id(board_idx))
 }
 
-fn to_unicode_char(piece: &Option<PieceOnBoard>) -> char {
+fn to_unicode_char(piece_kind: PieceKind, force: Force) -> char {
     use self::PieceKind::*;
     use self::Force::*;
-    match piece {
-        Some(piece) => match (piece.force, piece.kind) {
-            (White, Pawn) => '♙',
-            (White, Knight) => '♘',
-            (White, Bishop) => '♗',
-            (White, Rook) => '♖',
-            (White, Queen) => '♕',
-            (White, King) => '♔',
-            (Black, Pawn) => '♟',
-            (Black, Knight) => '♞',
-            (Black, Bishop) => '♝',
-            (Black, Rook) => '♜',
-            (Black, Queen) => '♛',
-            (Black, King) => '♚',
-        },
-        None => ' ',
+    match (force, piece_kind) {
+        (White, Pawn) => '♙',
+        (White, Knight) => '♘',
+        (White, Bishop) => '♗',
+        (White, Rook) => '♖',
+        (White, Queen) => '♕',
+        (White, King) => '♔',
+        (Black, Pawn) => '♟',
+        (Black, Knight) => '♞',
+        (Black, Bishop) => '♝',
+        (Black, Rook) => '♜',
+        (Black, Queen) => '♛',
+        (Black, King) => '♚',
     }
 }
