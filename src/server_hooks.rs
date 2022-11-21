a88267bf41080ea6aa006ef09940aaf131ccdb19
Andrei Matveiakin <a.matveiakin@gmail.com>
2022-11-21 21:31:23 +0400

    Fix Rust warnings

--- src/server_hooks.rs
+++ src/server_hooks.rs
@@ -8,6 +8,6 @@ pub trait ServerHooks {
 pub struct NoopServerHooks {}
 
 impl ServerHooks for NoopServerHooks {
-    fn on_event(&mut self, event: &BughouseServerEvent, game: Option<&GameState>, round: usize) {
+    fn on_event(&mut self, _event: &BughouseServerEvent, _game: Option<&GameState>, _round: usize) {
     }
 }
