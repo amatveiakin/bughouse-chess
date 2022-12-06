8256c1dd06c551fb52b5ed59a0fd1b91b16a462d
Andrei Matveiakin <a.matveiakin@gmail.com>
2022-12-06 12:28:57 +0400

    Upgrade all cargo dependencies
    $ cargo upgrade --incompatible

--- bughouse_console/src/main.rs
+++ bughouse_console/src/main.rs
@@ -50,7 +50,7 @@ fn main() -> io::Result<()> {
                 .arg(arg!(--starting_time [TIME] "Starting time for each player")
                     .default_value("5:00"))
                 .arg(arg!(--teaming [TEAMING] "How players are split into teams")
-                    .possible_values(["fixed", "dynamic"]).default_value("fixed"))
+                    .value_parser(["fixed", "dynamic"]).default_value("fixed"))
                 .arg(arg!(--sqlite_db [DB] "Path to an sqlite database file"))
         )
         .subcommand(
@@ -58,24 +58,24 @@ fn main() -> io::Result<()> {
                 .about("Run as client")
                 .arg(arg!(<server_address> "Server address"))
                 .arg(arg!(<player_name> "Player name"))
-                .arg(arg!([team] "Team").possible_values(["red", "blue"]))
+                .arg(arg!([team] "Team").value_parser(["red", "blue"]))
         )
         .get_matches();
 
     match matches.subcommand() {
         Some(("server", sub_matches)) => {
             server_main::run(server_main::ServerConfig {
-                teaming: sub_matches.value_of("teaming").unwrap().to_string(),
-                starting_time: sub_matches.value_of("starting_time").unwrap().to_string(),
-                sqlite_db: sub_matches.value_of("sqlite_db").map(|x| x.to_string()),
+                teaming: sub_matches.get_one::<String>("teaming").unwrap().clone(),
+                starting_time: sub_matches.get_one::<String>("starting_time").unwrap().clone(),
+                sqlite_db: sub_matches.get_one::<String>("sqlite_db").cloned(),
             });
             Ok(())
         },
         Some(("client", sub_matches)) => {
             client_main::run(client_main::ClientConfig {
-                server_address: sub_matches.value_of("server_address").unwrap().to_string(),
-                player_name: sub_matches.value_of("player_name").unwrap().to_string(),
-                team: sub_matches.value_of("team").map(|s| s.to_string()),
+                server_address: sub_matches.get_one::<String>("server_address").unwrap().clone(),
+                player_name: sub_matches.get_one::<String>("player_name").unwrap().clone(),
+                team: sub_matches.get_one::<String>("team").cloned(),
             })
         },
         _ => unreachable!("Exhausted list of subcommands and subcommand_required prevents `None`"),
