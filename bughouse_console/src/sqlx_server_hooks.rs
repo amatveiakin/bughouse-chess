a78c4a8a2fb5624bd7edb8abbde638f774b72ed8
Konstantin Mandelbaum <kmandelbaum@google.com>
2022-12-10 03:54:55 +0000

    Move the DB operations to sqlx, keeping the sqlite DB for now.
    Removed dependency on rusqlite.

--- bughouse_console/src/sqlx_server_hooks.rs
+++ bughouse_console/src/sqlx_server_hooks.rs
@@ -4,68 +4,61 @@
 // TODO: Benchmark SQL write speed.
 // TODO: More structured way to map data between Rust types and SQL;
 //   consider https://crates.io/crates/sea-orm.
+// TODO: insert a consistent row id, not to rely on implementation-specific
+//   columns, such as ROWID in sqlite.
 
 use log::error;
+use sqlx::prelude::*;
+use time::OffsetDateTime;
 
 use bughouse_chess::persistence::*;
 use bughouse_chess::server::*;
 use bughouse_chess::server_hooks::ServerHooks;
 use bughouse_chess::*;
 
-pub struct RusqliteServerHooks {
+pub struct SqlxServerHooks<DB: sqlx::Database> {
     invocation_id: String,
     game_start_time: Option<time::OffsetDateTime>,
-    conn: rusqlite::Connection,
+    pool: sqlx::Pool<DB>,
 }
 
-impl RusqliteServerHooks {
+impl SqlxServerHooks<sqlx::Sqlite>
+{
     pub fn new(address: &str) -> Result<Self, Box<dyn std::error::Error>> {
-        let conn = rusqlite::Connection::open(address)?;
-        // TODO: Include contest_id in finished_games.
-        conn.execute(
-            "CREATE TABLE IF NOT EXISTS finished_games (
-                git_version TEXT,
-                invocation_id TEXT,
-                game_start_time TIMESTAMP,
-                game_end_time TIMESTAMP,
-                player_red_a TEXT,
-                player_red_b TEXT,
-                player_blue_a TEXT,
-                player_blue_b TEXT,
-                result TEXT,
-                game_pgn TEXT)",
-            (),
-        )?;
-        conn.execute(
-            "CREATE TABLE IF NOT EXISTS client_performance (
-                git_version TEXT,
-                invocation_id TEXT,
-                user_agent TEXT,
-                time_zone TEXT,
-                turn_confirmation_p50,
-                turn_confirmation_p90,
-                turn_confirmation_p99,
-                turn_confirmation_n,
-                process_outgoing_events_p99,
-                process_notable_events_p99,
-                refresh_p99,
-                update_state_p50,
-                update_state_p90,
-                update_state_p99,
-                update_state_n,
-                update_clock_p99,
-                update_drag_state_p99)",
-            (),
-        )?;
+        let pool =
+            async_std::task::block_on(sqlx::SqlitePool::connect(&format!("{address}")))?;
+        Self::create_tables(&pool)?;
         Ok(Self {
             invocation_id: uuid::Uuid::new_v4().to_string(),
             game_start_time: None,
-            conn,
+            pool,
         })
     }
 }
 
-impl ServerHooks for RusqliteServerHooks {
+impl SqlxServerHooks<sqlx::Postgres>
+{
+    pub fn new(address: &str) -> Result<Self, Box<dyn std::error::Error>> {
+        let pool =
+            async_std::task::block_on(sqlx::Pool::<sqlx::Postgres>::connect(&format!("{address}")))?;
+        Self::create_tables(&pool)?;
+        Ok(Self {
+            invocation_id: uuid::Uuid::new_v4().to_string(),
+            game_start_time: None,
+            pool,
+        })
+    }
+}
+
+impl<DB: sqlx::Database> ServerHooks for SqlxServerHooks<DB>
+where
+    String: Type<DB> + for<'q>  Encode<'q, DB>,
+    i64: Type<DB> + for<'q> Encode<'q, DB>,
+    Option<i64>: Type<DB> + for<'q> Encode<'q, DB>,
+    Option<OffsetDateTime>: Type<DB> + for<'q> Encode<'q, DB>,
+    for<'c> &'c mut DB::Connection: sqlx::Executor<'c, Database = DB>,
+    for<'a> <DB as sqlx::database::HasArguments<'a>>::Arguments: sqlx::IntoArguments<'a, DB>,
+{
     fn on_client_event(&mut self, event: &BughouseClientEvent) {
         if let BughouseClientEvent::ReportPerformace(performance) = event {
             self.record_client_performance(&performance)
@@ -84,7 +77,56 @@ impl ServerHooks for RusqliteServerHooks {
     }
 }
 
-impl RusqliteServerHooks {
+impl<DB: sqlx::Database> SqlxServerHooks<DB>
+where
+    String: Type<DB> + for<'q>  Encode<'q, DB>,
+    i64: Type<DB> + for<'q> Encode<'q, DB>,
+    Option<i64>: Type<DB> + for<'q> Encode<'q, DB>,
+    Option<OffsetDateTime>: Type<DB> + for<'q> Encode<'q, DB>,
+    for<'c> &'c mut DB::Connection: sqlx::Executor<'c, Database = DB>,
+    for<'a> <DB as sqlx::database::HasArguments<'a>>::Arguments: sqlx::IntoArguments<'a, DB>,
+{
+    fn create_tables(pool: &sqlx::Pool<DB>) -> Result<(), Box<dyn std::error::Error>> {
+        async_std::task::block_on(
+            sqlx::query(
+                "CREATE TABLE IF NOT EXISTS finished_games (
+                git_version TEXT,
+                invocation_id TEXT,
+                game_start_time TIMESTAMP,
+                game_end_time TIMESTAMP,
+                player_red_a TEXT,
+                player_red_b TEXT,
+                player_blue_a TEXT,
+                player_blue_b TEXT,
+                result TEXT,
+                game_pgn TEXT)",
+            )
+            .execute(pool),
+        )?;
+        async_std::task::block_on(
+            sqlx::query(
+                "CREATE TABLE IF NOT EXISTS client_performance (
+                git_version TEXT,
+                invocation_id TEXT,
+                user_agent TEXT,
+                time_zone TEXT,
+                turn_confirmation_p50 INTEGER,
+                turn_confirmation_p90 INTEGER,
+                turn_confirmation_p99 INTEGER,
+                turn_confirmation_n INTEGER,
+                process_outgoing_events_p99 INTEGER,
+                process_notable_events_p99 INTEGER,
+                refresh_p99 INTEGER,
+                update_state_p50 INTEGER,
+                update_state_p90 INTEGER,
+                update_state_p99 INTEGER,
+                update_state_n INTEGER,
+                update_clock_p99 INTEGER,
+                update_drag_state_p99 INTEGER)",
+            )
+            .execute(pool))?;
+        Ok(())
+    }
     fn record_game_finish(
         &mut self,
         event: &BughouseServerEvent,
@@ -94,8 +136,9 @@ impl RusqliteServerHooks {
         let Some(row) = self.game_result(event, maybe_game, round) else {
             return;
         };
-        let execute_result = self.conn.execute(
-            "INSERT INTO finished_games (
+        let execute_result = async_std::task::block_on(
+            sqlx::query(
+                "INSERT INTO finished_games (
                 git_version,
                 invocation_id,
                 game_start_time,
@@ -106,19 +149,19 @@ impl RusqliteServerHooks {
                 player_blue_b,
                 result,
                 game_pgn)
-                VALUES (?1, ?2, ?3, ?4, ?5, ?6, ?7, ?8, ?9, ?10)",
-            (
-                row.git_version,
-                row.invocation_id,
-                row.game_start_time,
-                row.game_end_time,
-                row.player_red_a,
-                row.player_red_b,
-                row.player_blue_a,
-                row.player_blue_b,
-                row.result,
-                row.game_pgn,
-            ),
+                VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)",
+            )
+            .bind(row.git_version)
+            .bind(row.invocation_id)
+            .bind(row.game_start_time)
+            .bind(row.game_end_time)
+            .bind(row.player_red_a)
+            .bind(row.player_red_b)
+            .bind(row.player_blue_a)
+            .bind(row.player_blue_b)
+            .bind(row.result)
+            .bind(row.game_pgn)
+            .execute(&self.pool),
         );
         if let Err(e) = execute_result {
             error!("Error persisting game result: {}", e);
@@ -134,7 +177,7 @@ impl RusqliteServerHooks {
         let update_state = stats.get("update_state");
         let update_clock = stats.get("update_clock");
         let update_drag_state = stats.get("update_drag_state");
-        let execute_result = self.conn.execute(
+        let execute_result = async_std::task::block_on(sqlx::query(
             "INSERT INTO client_performance (
                 git_version,
                 invocation_id,
@@ -153,27 +196,25 @@ impl RusqliteServerHooks {
                 update_state_n,
                 update_clock_p99,
                 update_drag_state_p99)
-                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
-            rusqlite::params![
-                my_git_version!().to_owned(),
-                self.invocation_id,
-                perf.user_agent,
-                perf.time_zone,
-                turn_confirmation.map(|s| s.p50),
-                turn_confirmation.map(|s| s.p90),
-                turn_confirmation.map(|s| s.p99),
-                turn_confirmation.map(|s| s.num_values),
-                process_outgoing_events.map(|s| s.p99),
-                process_notable_events.map(|s| s.p99),
-                refresh.map(|s| s.p99),
-                update_state.map(|s| s.p50),
-                update_state.map(|s| s.p90),
-                update_state.map(|s| s.p99),
-                update_state.map(|s| s.num_values),
-                update_clock.map(|s| s.p99),
-                update_drag_state.map(|s| s.p99),
-            ]
-        );
+                VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17)")
+                .bind(my_git_version!().to_owned())
+                .bind(self.invocation_id.clone())
+                .bind(perf.user_agent.clone())
+                .bind(perf.time_zone.clone())
+                .bind(turn_confirmation.map(|s| s.p50 as i64))
+                .bind(turn_confirmation.map(|s| s.p90 as i64))
+                .bind(turn_confirmation.map(|s| s.p99 as i64))
+                .bind(turn_confirmation.map(|s| s.num_values as i64))
+                .bind(process_outgoing_events.map(|s| s.p99 as i64))
+                .bind(process_notable_events.map(|s| s.p99 as i64))
+                .bind(refresh.map(|s| s.p99 as i64))
+                .bind(update_state.map(|s| s.p50 as i64))
+                .bind(update_state.map(|s| s.p90 as i64))
+                .bind(update_state.map(|s| s.p99 as i64))
+                .bind(update_state.map(|s| s.num_values as i64))
+                .bind(update_clock.map(|s| s.p99 as i64))
+                .bind(update_drag_state.map(|s| s.p99 as i64))
+            .execute(&self.pool));
         if let Err(e) = execute_result {
             error!("Error persisting client performance: {}", e);
         }
@@ -200,14 +241,14 @@ impl RusqliteServerHooks {
         Some(GameResultRow {
             git_version: my_git_version!().to_owned(),
             invocation_id: self.invocation_id.to_string(),
-            game_start_time: self.game_start_time.map(|x| x.unix_timestamp()),
-            game_end_time: Some(time::OffsetDateTime::now_utc().unix_timestamp()),
+            game_start_time: self.game_start_time,
+            game_end_time: Some(time::OffsetDateTime::now_utc()),
             player_red_a: players.0,
             player_red_b: players.1,
             player_blue_a: players.2,
             player_blue_b: players.3,
             result,
-            game_pgn: pgn::export_to_bpgn(pgn::BughouseExportFormat{}, game.game(), round),
+            game_pgn: pgn::export_to_bpgn(pgn::BughouseExportFormat {}, game.game(), round),
         })
     }
 }
