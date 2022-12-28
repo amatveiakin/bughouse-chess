amatveiakin/bughouse-chess

commit 5c42d4207cb5dbae463e28a3292f88c564d711c4

Add turn log

bughouse_wasm/src/lib.rs
@@ -598,6 +598,7 @@ impl WebClient {
                     .map(|record| &record.turn_expanded);
                 self.set_turn_highlights("pre", pre_turn_highlight, display_board_idx)?;
             }
+            update_turn_log(&game, board_idx, display_board_idx)?;
         }
         if is_clock_ticking(&game, my_id) {
             document.body()?.class_list().add_1("active-player")?
@@ -754,6 +755,9 @@ impl WebDocument {
         Ok(element)
     }
 
+    pub fn create_element(&self, local_name: &str) -> JsResult<web_sys::Element> {
+        self.0.create_element(local_name)
+    }
     pub fn create_svg_element(&self, local_name: &str) -> JsResult<web_sys::Element> {
         self.0.create_element_ns(Some("http://www.w3.org/2000/svg"), local_name)
     }
@@ -769,6 +773,16 @@ fn remove_all_children(node: &web_sys::Node) -> JsResult<()> {
     }
     Ok(())
 }
+
+fn is_scrolled_to_bottom(e: &web_sys::Element) -> bool {
+    let eps = 1;
+    e.scroll_top() >= e.scroll_height() - e.client_height() - eps
+}
+
+fn scroll_to_bottom(e: &web_sys::Element) {
+    e.set_scroll_top(e.scroll_height() - e.client_height());
+}
+
 
 #[wasm_bindgen]
 pub fn init_page() -> JsResult<()> {
@@ -1037,6 +1051,32 @@ fn render_grids(perspective: Perspective) -> JsResult<()> {
     Ok(())
 }
 
+fn update_turn_log(
+    game: &BughouseGame, board_idx: BughouseBoard, display_board_idx: DisplayBoard
+) -> JsResult<()> {
+    let document = web_document();
+    let log_container_node = document.get_existing_element_by_id(&turn_log_container_node_id(display_board_idx))?;
+    let log_node = document.get_existing_element_by_id(&turn_log_node_id(display_board_idx))?;
+    let was_at_bottom = is_scrolled_to_bottom(&log_container_node);
+    remove_all_children(&log_node)?;
+    for record in game.turn_log().iter() {
+        if record.player_id.board_idx == board_idx {
+            let force = force_id(record.player_id.force);
+            let node = document.create_element("div")?;
+            node.set_text_content(Some(&record.to_log_entry()));
+            node.set_attribute("class", &format!("turn-record turn-record-{force}"))?;
+            log_node.append_child(&node)?;
+        }
+    }
+    // Keep log scrolled to bottom if it's already there. It's also possible to snap scrolling
+    // with CSS `scroll-snap-type` (https://stackoverflow.com/a/60546366/3092679), but the snap
+    // range is too large (especially in Firefox), so it becomes very hard to browse the log.
+    if was_at_bottom {
+        scroll_to_bottom(&log_container_node);
+    }
+    Ok(())
+}
+
 fn setup_participation_mode(observer: bool) -> JsResult<()> {
     let body = web_document().body()?;
     if observer {
@@ -1179,6 +1219,13 @@ fn turn_highlights(turn_expanded: &TurnExpanded) -> Vec<(&'static str, Coord)> {
     highlights
 }
 
+fn force_id(force: Force) -> &'static str {
+    match force {
+        Force::White => "white",
+        Force::Black => "black",
+    }
+}
+
 fn board_id(idx: DisplayBoard) -> &'static str {
     match idx {
         DisplayBoard::Primary => "primary",
@@ -1219,6 +1266,14 @@ fn reserve_container_id(board_idx: DisplayBoard, player_idx: DisplayPlayer) -> S
 
 fn reserve_node_id(board_idx: DisplayBoard, player_idx: DisplayPlayer) -> String {
     format!("reserve-group-{}-{}", board_id(board_idx), player_id(player_idx))
+}
+
+fn turn_log_container_node_id(board_idx: DisplayBoard) -> String {
+    format!("turn-log-container-{}", board_id(board_idx))
+}
+
+fn turn_log_node_id(board_idx: DisplayBoard) -> String {
+    format!("turn-log-{}", board_id(board_idx))
 }
 
 fn piece_layer_id(board_idx: DisplayBoard) -> String {

bughouse_console/src/tui.rs
@@ -56,7 +56,7 @@ fn render_reserve(reserve: &Reserve, force: Force) -> String {
     let mut stacks = Vec::new();
     for (piece_kind, &amount) in reserve.iter() {
         if amount > 0 {
-            stacks.push(String::from(to_unicode_char(piece_kind, force)).repeat(amount.into()));
+            stacks.push(String::from(piece_to_pictogram(piece_kind, force)).repeat(amount.into()));
         }
     }
     format!(
@@ -115,7 +115,7 @@ fn render_grid(grid: &Grid, orientation: BoardOrientation) -> String {
                     let color_idx = (coord.row.to_zero_based() + coord.col.to_zero_based()) % 2;
                     colors[usize::from(color_idx)].apply_to(
                         format_square(match grid[coord] {
-                            Some(piece) => to_unicode_char(piece.kind, piece.force),
+                            Some(piece) => piece_to_pictogram(piece.kind, piece.force),
                             None => ' ',
                         })
                     ).to_string()
@@ -130,23 +130,4 @@ fn render_grid(grid: &Grid, orientation: BoardOrientation) -> String {
 
 fn format_square(ch: char) -> String {
     format!(" {} ", ch)
-}
-
-fn to_unicode_char(piece_kind: PieceKind, force: Force) -> char {
-    use self::PieceKind::*;
-    use self::Force::*;
-    match (force, piece_kind) {
-        (White, Pawn) => '♙',
-        (White, Knight) => '♘',
-        (White, Bishop) => '♗',
-        (White, Rook) => '♖',
-        (White, Queen) => '♕',
-        (White, King) => '♔',
-        (Black, Pawn) => '♟',
-        (Black, Knight) => '♞',
-        (Black, Bishop) => '♝',
-        (Black, Rook) => '♜',
-        (Black, Queen) => '♛',
-        (Black, King) => '♚',
-    }
 }

src/board.rs
@@ -6,7 +6,7 @@ use std::collections::HashMap;
 use std::rc::Rc;
 
 use enum_map::{EnumMap, enum_map};
-use itertools::Itertools;
+use itertools::{Itertools, iproduct};
 use serde::{Serialize, Deserialize};
 
 use crate::once_cell_regex;
@@ -1042,34 +1042,60 @@ impl Board {
 
     // Renders turn as algebraic notation, PGN-style, see
     //   http://www.saremba.de/chessgml/standards/pgn/pgn-complete.htm
-    // Improvement potential. Omit `from` square when possible.
-    // Improvement potential. Allow to choose formatting style (e.g. "0-0" or "O-O").
-    pub fn turn_to_algebraic(&self, turn: Turn) -> Result<String, TurnError> {
-        let notation = self.turn_to_algebraic_impl(turn)?;
+    //
+    // TODO: Check and mate annotations.
+    // TODO: Formatting options:
+    //   - Short or long algebraic;
+    //   - Unicode: None / Just characters / Characters and pieces;
+    //   Allow to specify options when exporting PGN.
+    pub fn turn_to_algebraic(&self, turn: Turn, mode: TurnMode) -> Result<String, TurnError> {
+        let notation = self.turn_to_algebraic_impl(turn, mode)?;
         // Improvement potential. Remove when sufficiently tested.
-        if let Ok(turn_parsed) = self.algebraic_to_turn(&notation, TurnMode::Normal) {
+        if let Ok(turn_parsed) = self.algebraic_to_turn(&notation, mode) {
             assert_eq!(turn_parsed, turn, "{}", notation);
         }
         Ok(notation)
     }
 
-    fn turn_to_algebraic_impl(&self, turn: Turn) -> Result<String, TurnError> {
+    fn turn_to_algebraic_impl(&self, turn: Turn, mode: TurnMode) -> Result<String, TurnError> {
         match turn {
             Turn::Move(mv) => {
-                let piece = self.grid[mv.from].ok_or(TurnError::PieceMissing)?;
-                let capture = get_capture(&self.grid, mv.from, mv.to, self.en_passant_target);
-                let promotion = match mv.promote_to {
-                    Some(piece_kind) => format!("={}", piece_kind.to_full_algebraic()),
-                    None => "".to_owned(),
-                };
-                Ok(format!(
-                    "{}{}{}{}{}",
-                    piece.kind.to_algebraic_for_move(),
-                    mv.from.to_algebraic(),
-                    if capture.is_some() { "x" } else { "" },
-                    mv.to.to_algebraic(),
-                    promotion,
-                ))
+                let mut error = None;
+                for (&include_col, &include_row) in iproduct!(&[false, true], &[false, true]) {
+                    let piece = self.grid[mv.from].ok_or(TurnError::PieceMissing)?;
+                    let capture = get_capture(&self.grid, mv.from, mv.to, self.en_passant_target);
+                    let promotion = match mv.promote_to {
+                        Some(piece_kind) => format!("={}", piece_kind.to_full_algebraic()),
+                        None => "".to_owned(),
+                    };
+                    let mut from = String::new();
+                    if include_col {
+                        from.push(mv.from.col.to_algebraic())
+                    };
+                    if include_row {
+                        from.push(mv.from.row.to_algebraic())
+                    };
+                    let algebraic = format!(
+                        "{}{}{}{}{}",
+                        piece.kind.to_algebraic_for_move(),
+                        from,
+                        if capture.is_some() { "×" } else { "" },
+                        mv.to.to_algebraic(),
+                        promotion,
+                    );
+                    match self.algebraic_to_turn(&algebraic, mode) {
+                        Err(e) => {
+                            error = Some(e);
+                        },
+                        Ok(turn_parsed) => {
+                            assert_eq!(turn_parsed, turn);
+                            return Ok(algebraic);
+                        }
+                    }
+                }
+                let error = error.unwrap();
+                assert_ne!(error, TurnError::AmbiguousNotation);
+                Err(error)
             },
             Turn::Drop(drop) => {
                 Ok(format!("{}@{}", drop.piece_kind.to_full_algebraic(), drop.to.to_algebraic()))

src/game.rs
@@ -7,10 +7,10 @@ use itertools::Itertools;
 use serde::{Serialize, Deserialize};
 use strum::{EnumIter, IntoEnumIterator};
 
-use crate::TurnFacts;
-use crate::board::{Board, Reserve, Turn, TurnInput, TurnExpanded, TurnMode, TurnError, ChessGameStatus, VictoryReason, DrawReason};
+use crate::board::{Board, Reserve, Turn, TurnInput, TurnExpanded, TurnFacts, TurnMode, TurnError, ChessGameStatus, VictoryReason, DrawReason};
 use crate::clock::GameInstant;
 use crate::force::Force;
+use crate::piece::piece_to_pictogram;
 use crate::player::Team;
 use crate::rules::{ChessRules, BughouseRules};
 use crate::starter::{EffectiveStartingPosition, generate_starting_position};
@@ -41,6 +41,20 @@ impl TurnRecordExpanded {
             player_id: self.player_id,
             turn_algebraic: self.turn_expanded.algebraic.clone(),
             time: self.time,
+        }
+    }
+
+    pub fn to_log_entry(&self) -> String {
+        let algebraic = &self.turn_expanded.algebraic;
+        let s = if let Some(capture) = self.turn_expanded.capture {
+            let capture = piece_to_pictogram(capture.piece_kind, capture.force);
+            format!("{algebraic} ·{capture}")
+        } else {
+            format!("{algebraic}")
+        };
+        match self.mode {
+            TurnMode::Normal => s,
+            TurnMode::Preturn => format!("({s})"),
         }
     }
 }
@@ -351,7 +365,7 @@ impl BughouseGame {
         let board = &mut self.boards[board_idx];
         let player_id = BughousePlayerId{ board_idx, force: board.turn_owner(mode) };
         let turn = board.parse_turn_input(turn_input, mode)?;
-        let turn_algebraic = board.turn_to_algebraic(turn)?;
+        let turn_algebraic = board.turn_to_algebraic(turn, mode)?;
         let turn_facts = board.try_turn(turn, mode, now)?;
         let other_board = &mut self.boards[board_idx.other()];
         match mode {

src/piece.rs
@@ -85,3 +85,22 @@ impl PieceKind {
         as_single_char(notation).and_then(Self::from_algebraic_char)
     }
 }
+
+pub fn piece_to_pictogram(piece_kind: PieceKind, force: Force) -> char {
+    use self::PieceKind::*;
+    use self::Force::*;
+    match (force, piece_kind) {
+        (White, Pawn) => '♙',
+        (White, Knight) => '♘',
+        (White, Bishop) => '♗',
+        (White, Rook) => '♖',
+        (White, Queen) => '♕',
+        (White, King) => '♔',
+        (Black, Pawn) => '♟',
+        (Black, Knight) => '♞',
+        (Black, Bishop) => '♝',
+        (Black, Rook) => '♜',
+        (Black, Queen) => '♛',
+        (Black, King) => '♚',
+    }
+}

www/index.html
@@ -40,11 +40,14 @@
       <svg id="reserve-primary-bottom"></svg>
       <div id="player-name-primary-top" class="player-name"></div>
       <div id="player-name-primary-bottom" class="player-name"></div>
-      <div id="clock-container-primary-top">
-        <div id="clock-primary-top" class="clock"></div>
+      <div id="clock-container-primary-top" class="clock-container">
+        <div id="clock-primary-top" class="clock clock-inactive">-:--</div>
       </div>
-      <div id="clock-container-primary-bottom">
-        <div id="clock-primary-bottom" class="clock"></div>
+      <div id="clock-container-primary-bottom" class="clock-container">
+        <div id="clock-primary-bottom" class="clock clock-inactive">-:--</div>
+      </div>
+      <div id="turn-log-container-primary" class="turn-log-container">
+        <div id="turn-log-primary" class="turn-log"></div>
       </div>
 
       <div id="score-team"></div>
@@ -57,11 +60,14 @@
       <svg id="reserve-secondary-bottom"></svg>
       <div id="player-name-secondary-top" class="player-name"></div>
       <div id="player-name-secondary-bottom" class="player-name"></div>
-      <div id="clock-container-secondary-top">
-        <div id="clock-secondary-top" class="clock"></div>
+      <div id="clock-container-secondary-top" class="clock-container">
+        <div id="clock-secondary-top" class="clock clock-inactive">-:--</div>
       </div>
-      <div id="clock-container-secondary-bottom">
-        <div id="clock-secondary-bottom" class="clock"></div>
+      <div id="clock-container-secondary-bottom" class="clock-container">
+        <div id="clock-secondary-bottom" class="clock clock-inactive">-:--</div>
+      </div>
+      <div id="turn-log-container-secondary" class="turn-log-container">
+        <div id="turn-log-secondary" class="turn-log"></div>
       </div>
 
       <div id="info-string-container">

commit 54118d987861e7d95462bd92a77123a1a97005bd

Speed up log scrolling
Fixes `update_state` performance regression introduced in
5c42d4207cb5dbae463e28a3292f88c564d711c4

bughouse_console/src/client_main.rs
@@ -261,8 +261,7 @@ pub fn run(config: ClientConfig) -> io::Result<()> {
                     execute!(stdout, terminal::Clear(terminal::ClearType::All))?;
                 },
                 NotableEvent::GameOver(..) => {},
-                NotableEvent::MyTurnMade => {},
-                NotableEvent::OpponentTurnMade => {},
+                NotableEvent::TurnMade(..) => {},
                 NotableEvent::MyReserveRestocked => {},
                 NotableEvent::LowTime => {},
                 NotableEvent::GameExportReady(..) => {

bughouse_wasm/src/lib.rs
@@ -118,7 +118,7 @@ impl JsMeter {
 }
 
 #[wasm_bindgen]
-pub struct JsEventMyNoop {}  // in contrast to `null`, indicates that event list is not over
+pub struct JsEventNoop {}  // in contrast to `null`, indicates that event list is not over
 
 #[wasm_bindgen]
 pub struct JsEventContestStarted { contest_id: String }
@@ -496,6 +496,9 @@ impl WebClient {
                 let is_observer = matches!(my_id, BughouseParticipantId::Observer(_));
                 render_grids(alt_game.perspective())?;
                 setup_participation_mode(is_observer)?;
+                for display_board_idx in DisplayBoard::iter() {
+                    scroll_log_to_bottom(display_board_idx)?;
+                }
                 Ok(JsEventGameStarted{}.into())
             },
             Some(NotableEvent::GameOver(game_status)) => {
@@ -505,8 +508,19 @@ impl WebClient {
                     SubjectiveGameResult::Draw => Ok(JsEventDraw{}.into()),
                 }
             },
-            Some(NotableEvent::MyTurnMade) => Ok(JsEventTurnMade{}.into()),
-            Some(NotableEvent::OpponentTurnMade) => Ok(JsEventTurnMade{}.into()),
+            Some(NotableEvent::TurnMade(player_id)) => {
+                let Some(GameState{ ref alt_game, .. }) = self.state.game_state() else {
+                    return Err(rust_error!("No game in progress"));
+                };
+                let display_board_idx = get_display_board_index(player_id.board_idx, alt_game.my_id());
+                scroll_log_to_bottom(display_board_idx)?;
+                if let BughouseParticipantId::Player(my_player_id) = alt_game.my_id() {
+                    if player_id.board_idx == my_player_id.board_idx {
+                        return Ok(JsEventTurnMade{}.into());
+                    }
+                }
+                Ok(JsEventNoop{}.into())
+            }
             Some(NotableEvent::MyReserveRestocked) => Ok(JsEventMyReserveRestocked{}.into()),
             Some(NotableEvent::LowTime) => Ok(JsEventLowTime{}.into()),
             Some(NotableEvent::GameExportReady(content)) => Ok(JsEventGameExportReady{ content }.into()),
@@ -796,13 +810,16 @@ fn remove_all_children(node: &web_sys::Node) -> JsResult<()> {
     Ok(())
 }
 
-fn is_scrolled_to_bottom(e: &web_sys::Element) -> bool {
-    let eps = 1;
-    e.scroll_top() >= e.scroll_height() - e.client_height() - eps
+fn scroll_to_bottom(e: &web_sys::Element) {
+    // Do not try to compute the real scroll position, as it is very slow!
+    // See the comment in `update_turn_log`.
+    e.set_scroll_top(1_000_000_000);
 }
 
-fn scroll_to_bottom(e: &web_sys::Element) {
-    e.set_scroll_top(e.scroll_height() - e.client_height());
+fn scroll_log_to_bottom(board_idx: DisplayBoard) -> JsResult<()> {
+    let e = web_document().get_existing_element_by_id(&turn_log_scroll_area_node_id(board_idx))?;
+    scroll_to_bottom(&e);
+    Ok(())
 }
 
 
@@ -1115,9 +1132,7 @@ fn update_turn_log(
     game: &BughouseGame, board_idx: BughouseBoard, display_board_idx: DisplayBoard
 ) -> JsResult<()> {
     let document = web_document();
-    let log_container_node = document.get_existing_element_by_id(&turn_log_scroll_area_node_id(display_board_idx))?;
     let log_node = document.get_existing_element_by_id(&turn_log_node_id(display_board_idx))?;
-    let was_at_bottom = is_scrolled_to_bottom(&log_container_node);
     remove_all_children(&log_node)?;
     for record in game.turn_log().iter() {
         if record.player_id.board_idx == board_idx {
@@ -1128,12 +1143,19 @@ fn update_turn_log(
             log_node.append_child(&node)?;
         }
     }
-    // Keep log scrolled to bottom if it's already there. It's also possible to snap scrolling
-    // with CSS `scroll-snap-type` (https://stackoverflow.com/a/60546366/3092679), but the snap
-    // range is too large (especially in Firefox), so it becomes very hard to browse the log.
-    if was_at_bottom {
-        scroll_to_bottom(&log_container_node);
-    }
+    // Note. The log will be scrolled to bottom whenever a turn is made on a given board (see
+    // `NotableEvent::TurnMade` handler). Another strategy would've been to keep the log scrolled
+    // to bottom if it was already there. I found two ways of doing this, but unfortunately none
+    // of them works well:
+    //   - This could done in JS, using this code to find if an element is at bottom:
+    //        e.scroll_top() >= e.scroll_height() - e.client_height() - 1
+    //     (as https://developer.mozilla.org/en-US/docs/Web/API/Element/scrollHeight#determine_if_an_element_has_been_totally_scrolled suggests)
+    //     But the test is very slow. It made the entire `update_state` an order of magnitued slower,
+    //     increasing update time from 1-10 ms to 10-100 ms.
+    //   - This could be done in CSS, via `scroll-snap-type`:
+    //       https://stackoverflow.com/a/60546366/3092679
+    //     but the snap range is too large (especially in Firefox), so it becomes very hard to browse
+    //     the log.
     Ok(())
 }
 

src/client.rs
@@ -10,7 +10,7 @@ use crate::chalk::{Chalkboard, ChalkCanvas, ChalkDrawing, ChalkMark};
 use crate::clock::{GameInstant, WallGameTimePair};
 use crate::display::{DisplayBoard, get_board_index};
 use crate::force::Force;
-use crate::game::{TurnRecord, BughouseParticipantId, BughouseObserserId, PlayerRelation, BughouseBoard, BughouseGameStatus, BughouseGame};
+use crate::game::{TurnRecord, BughouseParticipantId, BughousePlayerId, BughouseObserserId, PlayerRelation, BughouseBoard, BughouseGameStatus, BughouseGame};
 use crate::event::{BughouseServerEvent, BughouseClientEvent, BughouseClientPerformance};
 use crate::heartbeat::{Heart, HeartbeatOutcome};
 use crate::meter::{Meter, MeterBox, MeterStats};
@@ -32,8 +32,7 @@ pub enum NotableEvent {
     ContestStarted(String),  // contains ContestID
     GameStarted,
     GameOver(SubjectiveGameResult),
-    MyTurnMade,
-    OpponentTurnMade,
+    TurnMade(BughousePlayerId),
     MyReserveRestocked,
     LowTime,
     GameExportReady(String),
@@ -256,6 +255,9 @@ impl ClientState {
     pub fn make_turn(&mut self, turn_input: TurnInput) -> Result<(), TurnError> {
         let game_state = self.game_state_mut().ok_or(TurnError::NoGameInProgress)?;
         let GameState{ ref mut alt_game, time_pair, ref mut awaiting_turn_confirmation_since, .. } = game_state;
+        let BughouseParticipantId::Player(my_player_id) = alt_game.my_id() else {
+            return Err(TurnError::NotPlayer);
+        };
         let now = Instant::now();
         let game_now = GameInstant::from_pair_game_maybe_active(*time_pair, now);
         let mode = alt_game.try_local_turn(turn_input.clone(), game_now)?;
@@ -263,7 +265,7 @@ impl ClientState {
             *awaiting_turn_confirmation_since = Some(now);
         }
         self.connection.send(BughouseClientEvent::MakeTurn{ turn_input });
-        self.notable_event_queue.push_back(NotableEvent::MyTurnMade);
+        self.notable_event_queue.push_back(NotableEvent::TurnMade(my_player_id));
         Ok(())
     }
 
@@ -439,15 +441,14 @@ impl ClientState {
         if alt_game.status() != BughouseGameStatus::Active {
             return Err(cannot_apply_event!("Cannot make turn {}: game over", turn_algebraic));
         }
+        let is_my_turn = alt_game.my_id() == BughouseParticipantId::Player(player_id);
         let now = Instant::now();
-        if let BughouseParticipantId::Player(my_player_id) = alt_game.my_id() {
-            if player_id == my_player_id {
-                // It's normal that the client is not awaiting confirmation, because preturns are not confirmed.
-                if let Some(t0) = awaiting_turn_confirmation_since {
-                    let d = now.duration_since(*t0);
-                    self.turn_confirmed_meter.record_duration(d);
-                    *awaiting_turn_confirmation_since = None;
-                }
+        if is_my_turn {
+            // It's normal that the client is not awaiting confirmation, because preturns are not confirmed.
+            if let Some(t0) = awaiting_turn_confirmation_since {
+                let d = now.duration_since(*t0);
+                self.turn_confirmed_meter.record_duration(d);
+                *awaiting_turn_confirmation_since = None;
             }
         }
         if time_pair.is_none() {
@@ -463,10 +464,10 @@ impl ClientState {
         })?;
         let new_reserve_size = my_reserve_size(alt_game);
         if generate_notable_events {
-            if let BughouseParticipantId::Player(my_player_id) = alt_game.my_id() {
-                if player_id == my_player_id.opponent() {
-                    self.notable_event_queue.push_back(NotableEvent::OpponentTurnMade);
-                }
+            if !is_my_turn {
+                // The `TurnMade` event fires when a turn is seen by the user, not when it's
+                // confirmed by the server.
+                self.notable_event_queue.push_back(NotableEvent::TurnMade(player_id));
             }
             if new_reserve_size > old_reserve_size {
                 self.notable_event_queue.push_back(NotableEvent::MyReserveRestocked);

www/index.js
@@ -453,7 +453,7 @@ function process_notable_events() {
     let js_event;
     while ((js_event = wasm_client().next_notable_event())) {
         const js_event_type = js_event?.constructor?.name;
-        if (js_event_type == 'JsEventMyNoop') {
+        if (js_event_type == 'JsEventNoop') {
             // Noop, but other events might be coming.
         } else if (js_event_type == 'JsEventContestStarted') {
             const url = new URL(window.location);