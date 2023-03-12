31ac4c65ae1c3e387825c83378528db54a744ee8
Konstantin Mandelbaum <kmandelbaum@google.com>
2023-03-12 00:46:45 +0000

    Implement remaining private database methods

--- bughouse_console/src/private_database.rs
+++ bughouse_console/src/private_database.rs
@@ -26,15 +26,15 @@ where
             .fetch_one(
                 sqlx::query::<DB>(
                     "SELECT
-                rowid,
-                creation_time,
-                user_name,
-                email,
-                password_hash,
-                registration_method
-             FROM accounts
-             WHERE
-                email=$1",
+                        rowid,
+                        creation_time,
+                        user_name,
+                        email,
+                        password_hash,
+                        registration_method
+                     FROM accounts
+                     WHERE
+                        email=$1",
                 )
                 .bind(email.to_owned()),
             )
@@ -51,8 +51,37 @@ where
             .ok_or(anyhow::Error::msg("failed to parse registration method"))?,
         })
     }
-    async fn account_by_user_name(&self, _user_name: &str) -> Result<Account, anyhow::Error> {
-        todo!()
+    async fn account_by_user_name(&self, user_name: &str) -> Result<Account, anyhow::Error> {
+        // TODO: handle NOT_FOUND separately from other errors.
+        let row = self
+            .pool
+            .fetch_one(
+                sqlx::query::<DB>(
+                    "SELECT
+                        rowid,
+                        creation_time,
+                        user_name,
+                        email,
+                        password_hash,
+                        registration_method
+                     FROM accounts
+                     WHERE
+                        user_name=$1",
+                )
+                .bind(user_name.to_owned()),
+            )
+            .await?;
+        Ok(Account {
+            id: Some(AccountId(row.try_get("rowid")?)),
+            creation_time: row.try_get("creation_time")?,
+            user_name: row.try_get("user_name")?,
+            email: row.try_get("email")?,
+            password_hash: row.try_get("password_hash")?,
+            registration_method: RegistrationMethod::try_from_string(
+                row.try_get("registration_method")?,
+            )
+            .ok_or(anyhow::Error::msg("failed to parse registration method"))?,
+        })
     }
 }
 
@@ -60,15 +89,16 @@ where
 impl<DB> PrivateDatabaseWriter for SqlxDatabase<DB>
 where
     DB: sqlx::Database + HasRowidColumnDefinition,
-    String: Type<DB> + for<'q> Encode<'q, DB>,
-    Option<String>: Type<DB> + for<'q> Encode<'q, DB>,
-    i64: Type<DB> + for<'q> Encode<'q, DB>,
+    for<'q> String: Type<DB> + Encode<'q, DB> + Decode<'q, DB>,
+    for<'q> Option<String>: Type<DB> + Encode<'q, DB> + Decode<'q, DB>,
+    for<'q> i64: Type<DB> + Encode<'q, DB> + Decode<'q, DB>,
     Option<i64>: Type<DB> + for<'q> Encode<'q, DB>,
-    OffsetDateTime: Type<DB> + for<'q> Encode<'q, DB>,
+    for<'q> OffsetDateTime: Type<DB> + Encode<'q, DB> + Decode<'q, DB>,
     Option<OffsetDateTime>: Type<DB> + for<'q> Encode<'q, DB>,
     bool: Type<DB> + for<'q> Encode<'q, DB>,
     for<'c> &'c mut DB::Connection: sqlx::Executor<'c, Database = DB>,
     for<'a> <DB as sqlx::database::HasArguments<'a>>::Arguments: sqlx::IntoArguments<'a, DB>,
+    for<'s> &'s str: sqlx::ColumnIndex<DB::Row>,
 {
     async fn create_tables(&self) -> anyhow::Result<()> {
         let rowid_column_definition = DB::ROWID_COLUMN_DEFINITION;
@@ -91,7 +121,7 @@ where
     }
     async fn create_account(&self, account: Account) -> anyhow::Result<()> {
         sqlx::query(
-            "INSERT into accounts(
+            "INSERT INTO accounts(
                 creation_time,
                 user_name,
                 email,
@@ -110,12 +140,59 @@ where
     }
     async fn update_account_txn(
         &self,
-        _id: AccountId,
-        _f: &(dyn FnOnce(&mut Account) + Send + Sync),
+        id: AccountId,
+        f: Box<dyn for<'a> FnOnce(&'a mut Account) + Send>,
     ) -> anyhow::Result<()> {
-        Err(anyhow::Error::msg(
-            "create_account is unimplemented in UnimplementedDatabase",
-        ))
+        let mut txn = self.pool.begin().await?;
+        let row = txn
+            .fetch_one(
+                sqlx::query::<DB>(
+                    "SELECT
+                        rowid,
+                        creation_time,
+                        user_name,
+                        email,
+                        password_hash,
+                        registration_method
+                     FROM accounts
+                     WHERE
+                        rowid=$1",
+                )
+                .bind(id.0),
+            )
+            .await?;
+        let mut account = Account {
+            id: Some(AccountId(row.try_get("rowid")?)),
+            creation_time: row.try_get("creation_time")?,
+            user_name: row.try_get("user_name")?,
+            email: row.try_get("email")?,
+            password_hash: row.try_get("password_hash")?,
+            registration_method: RegistrationMethod::try_from_string(
+                row.try_get("registration_method")?,
+            )
+            .ok_or(anyhow::Error::msg("failed to parse registration method"))?,
+        };
+        f(&mut account);
+        txn.execute(
+            sqlx::query(
+                "UPDATE accounts SET
+                    creation_time=$1,
+                    user_name=$2,
+                    email=$3,
+                    password_hash=$4,
+                    registration_method=$5
+                WHERE rowid=$6",
+            )
+            .bind(account.creation_time)
+            .bind(account.user_name)
+            .bind(account.email)
+            .bind(account.password_hash)
+            .bind(account.registration_method.to_string())
+            .bind(id.0),
+        )
+        .await?;
+        txn.commit().await?;
+        Ok(())
     }
 }
 
@@ -151,7 +228,7 @@ impl PrivateDatabaseWriter for UnimplementedDatabase {
     async fn update_account_txn(
         &self,
         _id: AccountId,
-        _f: &(dyn FnOnce(&mut Account) + Send + Sync),
+        _f: Box<dyn for<'a> FnOnce(&'a mut Account) + Send>,
     ) -> anyhow::Result<()> {
         Err(anyhow::Error::msg(
             "create_account is unimplemented in UnimplementedDatabase",
