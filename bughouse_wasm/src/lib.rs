15e1bd9c9728187059d1c64a0bfd7d5d10f9024b
Andrei Matveiakin <a.matveiakin@gmail.com>
2023-10-05 11:37:55 +0400

    Adjust promotion target selector size

--- bughouse_wasm/src/lib.rs
+++ bughouse_wasm/src/lib.rs
@@ -1529,8 +1529,10 @@ fn render_upgrade_promotion_selector(
 ) -> JsResult<()> {
     use std::f64::consts::PI;
     const PIECE_SIZE: f64 = 1.0;
-    const INNER_RADIUS: f64 = 0.65;
-    const OUTER_RADIUS: f64 = 1.6;
+    // Make central circle in promotion UI cover the entire square. Of course, 0.7 is slightly less
+    // than sqrt(2), but there's also stroke width.
+    const INNER_RADIUS: f64 = 0.7;
+    const OUTER_RADIUS: f64 = 1.7;
     const MID_RADIUS: f64 = (INNER_RADIUS + OUTER_RADIUS) / 2.0;
 
     let document = web_document();
