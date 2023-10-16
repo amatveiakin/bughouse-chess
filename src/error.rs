4383a927b0d630838d8e850ec8980c2755b69d01
Andrei Matveiakin <a.matveiakin@gmail.com>
2023-10-17 00:04:06 +0400

    Include git version in internal error messages

--- src/error.rs
+++ src/error.rs
@@ -1,9 +1,11 @@
 #[macro_export]
 macro_rules! internal_error_message {
     () => {
-        format!("Internal error at {}:{}.", file!(), line!())
+        format!("Internal error at {}:{} in {}.", file!(), line!(), $crate::my_git_version!())
     };
     ($($arg:tt)+) => {
-        format!("Internal error at {}:{}: {}.", file!(), line!(), format!($($arg)*))
+        format!(
+            "Internal error at {}:{} in {}: {}.",
+            file!(), line!(), $crate::my_git_version!(), format!($($arg)*))
     };
 }
