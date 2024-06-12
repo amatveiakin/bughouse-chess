d2a5c15e0e48edbc5135ce9134e5b4f0a0618a8e
Andrei Matveiakin <a.matveiakin@gmail.com>
2024-06-12 22:45:07 +0400

    Remove html suffix from URLs

--- www/webpack.config.js
+++ www/webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
   plugins: [
     new CopyWebpackPlugin({
       patterns: [
-        "*.html",
+        "**/index.html",
         "info-page.css",
         {
           from: "../assets/logo.svg",
