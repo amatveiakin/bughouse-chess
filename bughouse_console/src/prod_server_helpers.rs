d6c9feab468658ffee12fce0431ab07f3cc96e59
Andrei Matveiakin <a.matveiakin@gmail.com>
2024-06-14 16:44:17 +0400

    Limit punctuation in player names

--- bughouse_console/src/prod_server_helpers.rs
+++ bughouse_console/src/prod_server_helpers.rs
@@ -1,5 +1,6 @@
 use bughouse_chess::server_helpers::ServerHelpers;
 use censor::Censor;
+use itertools::Itertools;
 
 use crate::censor::profanity_censor;
 
@@ -58,6 +59,15 @@ pub fn validate_player_name(name: &str) -> Result<(), String> {
         // user ID. Also it just makes sense.
         return Err(format!("Player name must contain at least one letter."));
     }
+    if !name.chars().tuple_windows().all(valid_consecutive_letters) {
+        return Err(format!("Player name cannot contain several punctuation marks in a row."));
+    }
+    if !name.chars().next().unwrap().is_ascii_alphanumeric() {
+        return Err(format!("Player name must start with a letter or number."));
+    }
+    if !name.chars().last().unwrap().is_ascii_alphanumeric() {
+        return Err(format!("Player name must end with a letter or number."));
+    }
     let len = name.chars().count();
     if len < MIN_NAME_LENGTH {
         return Err(format!("Minimum name length is {MIN_NAME_LENGTH}."));
@@ -78,6 +88,10 @@ fn contains_ignoring_ascii_case(haystack: &[&str], needle: &str) -> bool {
     haystack.iter().any(|&s| s.eq_ignore_ascii_case(needle))
 }
 
+fn valid_consecutive_letters((a, b): (char, char)) -> bool {
+    a.is_ascii_alphanumeric() || b.is_ascii_alphanumeric()
+}
+
 
 pub struct ProdServerHelpers;
 
@@ -85,4 +99,34 @@ impl ServerHelpers for ProdServerHelpers {
     // Validates player name. Simple tests (such as length and character set) are also done
     // on the client.
     fn validate_player_name(&self, name: &str) -> Result<(), String> { validate_player_name(name) }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn validate_player_name_all() {
+        validate_player_name("a").unwrap_err(); // too short
+        validate_player_name("123456789o123456789o").unwrap();
+        validate_player_name("123456789o123456789o1").unwrap_err(); // too long
+
+        validate_player_name("ab").unwrap();
+        validate_player_name("12").unwrap_err(); // no letters
+        validate_player_name("a1").unwrap();
+        validate_player_name("a#").unwrap_err(); // illegal characters
+        validate_player_name("игрок").unwrap_err(); // illegal characters
+
+        validate_player_name("some").unwrap_err(); // reserved word
+        validate_player_name("some_player").unwrap(); // reserved word inside
+        validate_player_name("admin").unwrap_err(); // banned word
+        validate_player_name("MainAdmin").unwrap_err(); // banned word inside
+
+        validate_player_name("Ok-c_oo_l-name").unwrap(); // special characters OK
+        validate_player_name("too-_-cool").unwrap_err(); // consecutive special characters
+        validate_player_name("still__bad").unwrap_err(); // trailing special characters
+        validate_player_name("_bad").unwrap_err(); // trailing special characters
+        validate_player_name("bad_").unwrap_err(); // trailing special characters
+    }
 }
