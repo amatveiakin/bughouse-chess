4e253608b743b81f6e99c33f43e0e9ac907dff68
Andrei Matveiakin <a.matveiakin@gmail.com>
2024-06-15 19:25:15 +0400

    Use same clippy config for entire workspace

--- src/lib.rs
+++ src/lib.rs
@@ -6,17 +6,6 @@
 #![feature(let_chains)]
 #![feature(type_alias_impl_trait)]
 #![feature(unboxed_closures)]
-// Clippy philosophy. The goal is to have zero warnings from `cargo clippy` on the main branch.
-// Thus silencing all warning that I don't want to fix now. These decisions could be revised.
-#![allow(
-    clippy::collapsible_else_if,
-    clippy::large_enum_variant,
-    clippy::manual_is_ascii_check,
-    clippy::new_without_default,
-    clippy::option_map_unit_fn,
-    clippy::too_many_arguments,
-    clippy::type_complexity
-)]
 
 pub mod algebraic;
 pub mod altered_game;
